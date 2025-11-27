6921190840cc54153cfeca634fb98994fac6f2d1
Chris Hexton <chris@getvero.com>
2025-11-27 15:48:12 +1100

    Update product feature component

--- src/components/snippets/ProductFeature.jsx
+++ src/components/snippets/ProductFeature.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { MjmlImage } from '@faire/mjml-react';
+import { MjmlText } from '@faire/mjml-react';
 import H3 from '../content/H3.jsx';
 
 // Section with an image followed by content
-const ProductFeature = ({ children, title, imageUrl, linkUrl, altText = '', padding = '0 0 16px 0' }) => {
+const ProductFeature = ({ children, title, imageUrl, linkUrl, altText = '', padding = '0 16px 16px 0' }) => {
   // Clone children and pass padding prop to them
   const childrenWithPadding = React.Children.map(children, child => {
     if (React.isValidElement(child)) {
@@ -22,6 +23,15 @@ const ProductFeature = ({ children, title, imageUrl, linkUrl, altText = '', padd
       />
       <H3 padding={padding}>{title}</H3>
       {childrenWithPadding}
+      <MjmlText
+        fontSize="15px"
+        lineHeight="1.8"
+        cssClass="paragraph"
+        color="#16184B"
+        padding={padding}
+      >
+        <a href={linkUrl}>Read the post →</a>
+      </MjmlText>
     </>
   );
 };
